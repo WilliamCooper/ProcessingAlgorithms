0f06b4f0e1f1595b4e5a3cfc3e5be00c7b5ebd2a
WilliamCooper <cooperw@ucar.edu>
2022-05-26 08:13:02 -0600

    Transferred searchForAVariable.R to be able to search on EOL. Otherwise, this is a continuation of two efforts: Expand the variable list and Appendix B, and translate html code to RMarkdown code. See Problems.Rnd for comments on the statis of the former.

---
+++ searchForAVariable.R
@@ -0,0 +1,101 @@
+# search for a variable or variable mask in netCDF filesD, including from recent projects:
+library(Ranadu)
+# VarSearch <- 'CONC_H2O_VXL'  # variable or pattern to find
+VarSearch <- 'DFRSTS'  # variable or pattern to find
+VarSearch <- 'GGSF_A'  # variable or pattern to find
+VarSearch <- 'GLATF_A'  # variable or pattern to find
+VarSearch <- 'HYB'  # variable or pattern to find
+VarSearch <- 'GTKAT'  # variable or pattern to find
+VarSearch <- 'GVSPD'  # variable or pattern to find
+VarSearch <- 'H2O_PIC'  # variable or pattern to find
+VarSearch <- 'CO_PIC'  # variable or pattern to find
+VarSearch <- 'HNO3'  # variable or pattern to find
+VarSearch <- 'IRB'  # variable or pattern to find
+VarSearch <- 'IWC1DC'  # variable or pattern to find
+VarSearch <- 'IWD'  # variable or pattern to find
+VarSearch <- 'LATF_A'  # variable or pattern to find
+VarSearch <- 'LI82'  # variable or pattern to find
+VarSearch <- 'MHDG3'  # variable or pattern to find
+VarSearch <- 'MRLH'  # variable or pattern to find
+VarSearch <- 'NO_ACD'  # variable or pattern to find
+# VarSearch <- 'XNOCAL'  # variable or pattern to find
+PDirs <- list.dirs(path='/scr/raf/Prod_Data', recursive = FALSE)
+PDirs <- list.dirs(path='/Data/Prod_Data', recursive = FALSE)
+Dirs <- list.dirs(path=DataDirectory(), recursive = FALSE)
+Dirs <- c(Dirs, PDirs)
+SFLG <- TRUE
+for (D in Dirs) {
+  # if (grepl('TORERO', D)) {SFLG <- FALSE}
+  # if (SFLG) {next}
+  # if (!grepl('ICE-T', D)) {next}
+  # if (!grepl('ICEBRIDGE2015', D)) {next}
+  if (grepl('DEEPWAVE.orig', D)) {next}
+  if (grepl('RICO', D)) {next}
+  if (grepl('HEFT-10', D)) {next}
+  print (sprintf ('processing %s', D))
+  FL <- list.files(D, '.*rf...nc')
+  if (length(FL) < 1) {next}
+  for (F in FL[1]) {
+    if (grepl('gz$', F)) {next}
+    print (sprintf('directory %s file %s', D, F))
+    if (grepl('CSET', D)) {
+      print(sprintf('CSET flight is %s', F))
+      if (grepl('rf08', F)) {next}
+    }
+    if (grepl('ECLIPSE2019', D)) {
+      print(sprintf('E2019 flight is %s', F))
+      if (grepl('rf01', F)) {next}
+    }
+    if (grepl('ICE-T', D)) {
+      print(sprintf('ICE-T flight is %s', F))
+      if (grepl('rf04', F)) {next}
+      if (grepl('rf13', F)) {next}
+    }
+    if (grepl('ICEBRIDGE2015', D)) {
+      print(sprintf('ICEBRIDGE flight is %s', F))
+      if (grepl('2015rf02', F)) {next}
+    }
+    if (grepl('IDEAS-4-GV', D)) {
+      print(sprintf('IDEAS-4-GV flight is %s', F))
+      if (grepl('^rf05', F)) {next}
+      if (grepl('^sid2', F)) {next}
+    }
+    if (grepl('IDEAS4_LAMS', D)) {
+      print(sprintf('IDEAS4_LAMS flight is %s', F))
+      if (grepl('rf08', F)) {next}
+    }
+    if (grepl('NOMADSS', D)) {
+      print(sprintf('NOMADSS flight is %s', F))
+      if (grepl('rf07', F)) {next}
+      if (grepl('rf08', F)) {next}
+      if (grepl('rf12', F)) {next}
+      if (grepl('rf14', F)) {next}
+      if (grepl('rf19', F)) {next}
+    }
+    if (grepl('PLOWS', D)) {
+      print(sprintf('PLOWS flight is %s', F))
+      if (grepl('rf18', F)) {next}
+    }
+    if (grepl('TORERO', D)) {
+      print(sprintf('TORERO flight is %s', F))
+      if (grepl('rf06', F)) {next}
+    }
+    if (grepl('PREDICT_LAMS', D)) {
+      print(sprintf('PREDICT_LAMS flight is %s', F))
+      if (grepl('rf04', F)) {next}
+      if (grepl('rf06', F)) {next}
+      if (grepl('rf07', F)) {next}
+      if (grepl('rf08', F)) {next}
+      if (grepl('rf09', F)) {next}
+      if (grepl('rf11', F)) {next}
+    }
+    VS <- DataFileInfo(sprintf('%s/%s', D, F), LLrange = FALSE)
+    if (any(grepl(VarSearch, VS$Variables))) {
+    # if (any(VarSearch == VS$Variables)) {
+      print (sprintf ('search matches in %s file %s from %s', D, F, VS$End))
+      system(sprintf('ncdump -h %s/%s |grep %s', D, F, VarSearch))
+    }
+  }
+}
+
+
